c7af118da301eced38d230288e60396156097a6b
yashmathur865 <yashmathur865@gmail.com>
2025-01-18 15:36:37 +0530

    Remove middle node in a linked list

---
+++ RemoveMiddleInLL.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+}Node;
+
+int isDigit(char ch){
+    if(ch>='0' && ch<='9'){
+        return 1;
+    }
+    return 0;
+}
+
+Node* createNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(!newNode){
+        printf("Memory Allocation Failed\n");
+        exit(1);
+    }
+    newNode->data=data;
+    newNode->next=NULL;
+    return newNode;
+}
+
+void insertAtEnd(Node **head,int data){
+    Node* newNode = createNode(data);
+    if(*head==NULL){
+        *head=newNode;
+        return;
+    }
+    Node* temp = *head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newNode;
+}
+
+void printList(Node **head){
+    Node *curr = *head;
+    while(curr!=NULL){
+        printf("%d ",curr->data);
+        curr=curr->next;
+    }
+}
+
+void removeMiddle(Node **head){
+    if(*head==NULL){
+        return;
+    }
+    Node* slow = *head;
+    Node* fast = *head;
+    Node* prev = NULL;
+    if((*head)->next==NULL){
+        *head=NULL;
+        return;
+    }
+
+    while(fast!= NULL && fast->next!= NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    prev->next=slow->next;
+    free(slow);
+}
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    Node* head = NULL;
+    char str[1000];
+    scanf("%[^\n]%*c",str);
+
+    int i=0;
+    while(str[i]!='\0'){
+        if(str[i]==' '){
+            i++;
+            continue;
+        }
+        if(str[i]=='-'){
+            i++;
+            continue;
+        }
+        if(isDigit(str[i])==1){
+            int num=0;
+            int sign=1;
+            if(i-1>=0){
+                if(str[i-1]=='-'){
+                    sign=-1;
+                }
+            }
+            while(isDigit(str[i])==1){
+                num=num*10 + (str[i]-'0');
+                i++;
+            }
+            num=num*sign;
+            insertAtEnd(&head,num);
+        }
+        i++;
+    }
+
+    removeMiddle(&head);
+    printList(&head);
+    return 0;
+}
